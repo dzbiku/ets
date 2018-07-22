2eee6188f6a9c1b8e0c42d0450c6aef086e0922b
OnlyOne <dzbik.konrad@gmail.com>
2018-07-22 18:56:09 +0200

    weather api

---
+++ OnlyOne.Model/Weather/WeatherApi.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlyOne.Model.Weather
+{
+    public class WeatherApi
+    {
+        public string ApiKey { get; set; }
+        public string CityName { get; set; }
+        public string CountryCode { get; set; }
+        public string HtmlAddress { get; set; }
+
+        public WeatherApi(string apiKey,string city,string countryCode, string htmlAddress)
+        {
+            ApiKey = apiKey;
+            CityName = city;
+            CountryCode = countryCode;
+            HtmlAddress = htmlAddress;
+        }
+    }
+}
