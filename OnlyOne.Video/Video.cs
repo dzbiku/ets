168d1618efa73bb76c9e18611ab53755ca0f6715
OnlyOne <dzbik.konrad@gmail.com>
2018-07-24 23:41:18 +0200

    dev- switch to .net framework

--- OnlyOne.Video/Video.cs
+++ OnlyOne.Video/Video.cs
@@ -30,35 +30,38 @@ namespace OnlyOne.Video
         }
 
 
-        //public void testedtest()
-        //{
-        //    Size dsize = new Size(640, 480);
-        //  //  Size dsize = new Size(704, 576);
+        public void testedtest()
+        {
+            Size dsize = new Size(640, 480);
 
-        //    // Opens a camera device
-        //    using (VideoCapture capture = new VideoCapture(0))
-        //        // Read movie frames and write them to VideoWriter 
-        //    //using (VideoWriter writer = new VideoWriter("out.avi", -1, 24, dsize)) //704576
-            
-        //    using (VideoWriter writer = new VideoWriter("out.mp4", FourCC.Default, 20, dsize, false))
-        //    using (Mat frame = new Mat())
-        //    using (Mat dst = new Mat())
-        //    {
-        //        Console.WriteLine("Converting each movie frames...");
-        //        while (true)
-        //        {
-        //            // Read image
-        //            capture.Read(frame);
-        //            if (frame.Empty())
-        //                break;
+            // Opens a camera device
+            using (VideoCapture capture = new VideoCapture(0))
+                // Read movie frames and write them to VideoWriter 
+            using (VideoWriter writer = new VideoWriter("out.avi", -1, 25, dsize))
+            using (Mat frame = new Mat())
+            using (Mat gray = new Mat())
+            using (Mat canny = new Mat())
+            using (Mat dst = new Mat())
+            {
+                Console.WriteLine("Converting each movie frames...");
+                while (true)
+                {
+                    // Read image
+                    capture.Read(frame);
+                    if (frame.Empty())
+                        break;
 
-        //            Console.CursorLeft = 0;
-        //            Console.Write("{0} / {1}", capture.PosFrames, capture.FrameCount);
+                    Console.CursorLeft = 0;
+                    Console.Write("{0} / {1}", capture.PosFrames, capture.FrameCount);
 
-        //            // Write mat to VideoWriter
-        //            writer.Write(dst);
-        //        }
-        //    }
-        //}
+                    // grayscale -> canny -> resize
+                    Cv2.CvtColor(frame, gray, ColorConversionCodes.BGR2GRAY);
+                    Cv2.Canny(gray, canny, 100, 180);
+                    Cv2.Resize(canny, dst, dsize, 0, 0, InterpolationFlags.Linear);
+                    // Write mat to VideoWriter
+                    writer.Write(dst);
+                }
+            }
+        }
     }
 }
